2260f2f221a4a883c20eb06a7e46346423657d4d
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-29 19:22:47 -0700

    python

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,25 @@
 def balance_parens(str):
-    pass
+    no_more_paren = False
+    bracket_is_open = False
+    final_string = ""
+
+    # Iterates through the string, and tracks whether the bracket is open or closed
+    for i in range(0, len(str)):
+        if str.find(")", i) == -1:
+            no_more_paren = True
+        if str[i] == "(" and no_more_paren == True:
+            continue
+        elif  str[i] == "(" and bracket_is_open == False:
+            bracket_is_open = True
+            final_string += "("
+        elif str[i] == "(" and bracket_is_open == True:
+            continue
+        elif str[i] == ")" and bracket_is_open == False:
+            continue
+        elif str[i] == ")" and bracket_is_open == True:
+            bracket_is_open = False
+            final_string += ")"
+        else:
+            final_string += str[i]
+    return final_string
+
